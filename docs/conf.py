599868a108c36eeb74ecbabaaeecec687c23d762
Peter Buchegger <peter@pbuchegger.at>
2025-04-21 18:34:09 +0200

    update

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,12 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
 
 project = "OE5XRX - Remote Station"
 copyright = "2025, Peter OE5BPA"
 author = "Peter OE5BPA"
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     "myst_parser",
+    "rtds_action",
 ]
 
 source_suffix = {
@@ -27,8 +19,10 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 language = "de"
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+rtds_action_github_repo = "OE5XRX/RemoteStation"
+rtds_action_path = "_static"
+rtds_action_artifact_prefix = "artifacts-for-"
+rtds_action_github_token = os.environ["GITHUB_TOKEN"]
